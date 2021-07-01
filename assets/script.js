33bbef6beea36898493bd20bd7cc67d3c6e6574c
asif-96 <shuyeb.dk@gmail.com>
2021-07-01 22:51:33 +0600

    working on files

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,33 @@ if(navClose){
         navMenu.classList.remove('show-menu');
     })
 }
+
+
+// ---------------- CHANGE BACKGROUND HEADER ---------
+function scrollHeader(){
+    const header = document.getElementById('header');
+    // When the scroll is greater then 100 viewport height
+    if(this.scrollY >= 100) {
+        header.classList.add('scroll-header'); 
+    }else {
+        header.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+});
+
+
